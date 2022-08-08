5f3fb8a817866702d77d2be5f5d1e583c7ad7993
Kamal <kamal.ame19@gmail.com>
2022-08-08 16:19:03 +0530

    new commit

--- new.js
+++ new.js
@@ -1,5 +1,9 @@
 console.log("Test");
 
-//Initialize repository.
-//check changes.
-//Commit changes - Save your changes to repository.
+//1.Initialize repository.
+//2.check changes.
+//3.Commit changes - Save your changes to repository.
+//by here your code is still on your machine.
+
+//github is online cloud storage to save your code. 
+//4.Publish/Push your changes. on github.
